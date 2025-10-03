6da642f0b4c65fa2afa1ddd143134a8715a73ae6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-10-03 20:42:42 +0530

    implement CRUD operations for notes with proper error handling and project validation

--- src/controllers/note.controllers.js
+++ src/controllers/note.controllers.js
@@ -1,11 +1,103 @@
+import { Note } from "../models/note.models.js";
+import { Project } from "../models/project.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 
-const createNote = asyncHandler(async (req, res) => {});
-const getNoteById = asyncHandler(async (req, res) => {});
-const getNotes = asyncHandler(async (req, res) => {});
-const updateNote = asyncHandler(async (req, res) => {});
-const deleteNote = asyncHandler(async (req, res) => {});
+const createNote = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const { content } = req.body;
+
+    const project = await Project.findById(projectId);
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    const note = await Note.create({
+        project: projectId,
+        createdBy: req.user?._id,
+        content,
+    });
+
+    const populatedNote = await Note.findById(note._id).populate(
+        "createdBy",
+        "username fullname, avatar",
+    );
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, populatedNote, "Note created succesfully"));
+});
+
+const getNoteById = asyncHandler(async (req, res) => {
+    const { noteId } = req.params;
+
+    const note = await Note.findById(noteId).populate(
+        "createdBy",
+        "username fullname, avatar",
+    );
+
+    if (!note) {
+        throw new ApiError(404, "Note not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, note, "Note fetched successfully"));
+});
+
+const getNotes = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    const notes = await Note.find({
+        project: projectId,
+    }).populate("createdBy", "username fullname, avatar");
+
+    if (notes.length === 0) {
+        throw new ApiResponse(200, [], "No project notes found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, notes, "Notes fetched successfully"));
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+    const { noteId } = req.params;
+    const { content } = req.body;
+
+    const existingNote = await Note.findById(noteId);
+    if (!existingNote) {
+        throw new ApiError(404, "Note not found");
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate(
+        noteId,
+        { content },
+        { new: true },
+    ).populate("createdBy", "username fullname, avatar");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedNote, "Note updated successfully"));
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+    const { noteId } = req.params;
+
+    const deletedNote = await Note.findByIdAndDelete(noteId);
+    if (!deletedNote) {
+        throw new ApiError(404, "Note not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Note deleted successfully"));
+});
 
 export { createNote, getNoteById, getNotes, updateNote, deleteNote };
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,6 +1,38 @@
 import { Router } from "express";
+import {
+    createNote,
+    getNoteById,
+    getNotes,
+    updateNote,
+    deleteNote,
+} from "../controllers/note.controllers.js";
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constant.js";
+import { validateProjectPermission } from "../middlewares/permission.middleware.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
+router
+    .route("/:projectId")
+    .post(
+        isLoggedIn,
+        validateProjectPermission([UserRolesEnum.PROJECT_ADMIN]),
+        createNote,
+    )
+    .get(isLoggedIn, validateProjectPermission(AvailableUserRoles), getNotes);
 
-export default router
+router
+    .route("/:projectId/n/:noteId")
+    .get(isLoggedIn, validateProjectPermission(AvailableUserRoles), getNoteById)
+    .put(
+        isLoggedIn,
+        validateProjectPermission([UserRolesEnum.PROJECT_ADMIN]),
+        updateNote,
+    )
+    .delete(
+        isLoggedIn,
+        validateProjectPermission([UserRolesEnum.PROJECT_ADMIN]),
+        deleteNote,
+    );
+
+export default router;
